392b6e44c5a7b6e9997ab26c8d8d91c35a84b863
arbasti <90555409+arbasti@users.noreply.github.com>
2024-05-28 18:25:13 +0200

    Delete logout.php

--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	session_unset(); // Afin de libérer tout les variables de la session
-	session_destroy(); // Afin de détruire la session
-
-	header("Location: accueil.php");
-	exit();
-?>
