296111fd95829403e1a0b846e9d572b27463ef0c
arbasti <90555409+arbasti@users.noreply.github.com>
2024-05-28 17:23:17 +0200

    Update search.php

--- search.php
+++ search.php
@@ -1,46 +1,36 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "medicare_BDD";
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "medicare_BDD";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}
 
-$search = isset($_GET['search']) ? $_GET['search'] : '';
-$specialization = isset($_GET['specialization']) ? $_GET['specialization'] : '';
+	$search = $_GET['search'];
 
-$sql = "SELECT ID_Médecin, Nom, Prénom, Spécialité FROM Médecin WHERE 1=1";
+	$sql = "SELECT ID_Médecin, Nom, Prénom, Spécialité FROM Médecin WHERE Nom LIKE '%$search%' OR Prénom LIKE '%$search%'";
+	$result = $conn->query($sql);
 
-if ($search) {
-    $sql .= " AND (Nom LIKE '%$search%' OR Prénom LIKE '%$search%')";
-}
+	$output = '';
 
-if ($specialization) {
-    $sql .= " AND Spécialité = '$specialization'";
-}
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$output .= '<div class="search-result">';
+			$output .= '<h3><a href="medecin.php?id=' . $row['ID_Médecin'] . '">' . $row['Prénom'] . ' ' . $row['Nom'] . '</a></h3>';
+			$output .= '<p>' . $row['Spécialité'] . '</p>';
+			$output .= '</div>';
+		}
+	} else {
+		$output = '<p>Aucun résultat trouvé</p>';
+	}
 
-$result = $conn->query($sql);
+	echo $output;
 
-$output = '';
-
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        $output .= '<div class="search-result">';
-        $output .= '<h3><a href="medecin.php?id=' . $row['ID_Médecin'] . '">' . $row['Prénom'] . ' ' . $row['Nom'] . '</a></h3>';
-        $output .= '<p>' . $row['Spécialité'] . '</p>';
-        $output .= '</div>';
-    }
-} else {
-    $output = '<p>Aucun résultat trouvé</p>';
-}
-
-echo $output;
-
-$conn->close();
+	$conn->close();
 ?>
