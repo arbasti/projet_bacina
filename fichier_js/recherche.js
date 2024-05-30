40608df276ac7079121d65894db128ce4007647a
Tim <131018615+TimotheeMariotte@users.noreply.github.com>
2024-05-30 15:35:05 +0200

    enfin

--- fichier_js/recherche.js
+++ fichier_js/recherche.js
@@ -1,34 +1,51 @@
-// Show/hide search bar
+// afficher ou cacher la barre de recherche
 $('#search-button').click(function()
 {
 	$('#search-bar').toggle();
-	$('#search-results').empty(); // Clear search results when toggling the search bar
+	$('#search-results').empty(); // fermeture des résultats de recherche
 });
 
-// Search functionality
+// fonction recherche
 $('#search-input').on('input', function()
 {
-	var query = $(this).val();
-	if (query.length > 2)
-	{
-		$.ajax(
-		{
-			url: 'search.php',
-			method: 'GET',
-			data: { search: query },
-			success: function(data)
-			{
-				$('#search-results').html(data);
-			}
-		});
-	}
-	else
-	{
-		$('#search-results').empty();
-	}
+    var query = $(this).val();
+    if (query.length > 2)
+    {
+        $.ajax(
+        {
+            url: '../fichier_php/search.php', // Chemin ajusté
+            method: 'GET',
+            data: { search: query },
+            success: function(data)
+            {
+                $('#search-results').html(data);
+            }
+        });
+    }
+    else
+    {
+        $('#search-results').empty();
+    }
 });
 
-// Filter functionality
+// bouton de filtre
+$('.filter-button').click(function()
+{
+    var specialization = $(this).data('specialization');
+    $.ajax(
+    {
+        url: '../fichier_php/search.php', // Chemin ajusté
+        method: 'GET',
+        data: { specialization: specialization },
+        success: function(data)
+        {
+            $('#search-results').html(data);
+        }
+    });
+});
+
+
+// bouton de filtre
 $('.filter-button').click(function()
 {
 	var specialization = $(this).data('specialization');
