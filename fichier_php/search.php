7be27d5ddeaab94d6e7c9bfada4a548780a9fc62
Tim <131018615+TimotheeMariotte@users.noreply.github.com>
2024-05-30 15:20:43 +0200

    Update search.php

--- fichier_php/search.php
+++ fichier_php/search.php
@@ -1,46 +1,51 @@
 <?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "medicare_BDD";
+include('config.php'); // ce fichier contient la connexion à la base de données
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
+$search = isset($_GET['search']) ? $_GET['search'] : '';
+$specialization = isset($_GET['specialization']) ? $_GET['specialization'] : '';
 
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
+$sql = "SELECT ID_Médecin, Nom, Prénom, Spécialité, Photo, Bureau, Téléphone, Email FROM Médecin WHERE 1=1";
 
-	$search = isset($_GET['search']) ? $_GET['search'] : '';
-	$specialization = isset($_GET['specialization']) ? $_GET['specialization'] : '';
+// condition de recherche par nom/prénom/spécialisation
+if ($search) {
+    $sql .= " AND (Nom LIKE '%$search%' OR Prénom LIKE '%$search%' OR Spécialité LIKE '%$search%')";
+}
 
-	$sql = "SELECT ID_Médecin, Nom, Prénom, Spécialité FROM Médecin WHERE 1=1";
+// condition de spécialisation
+if ($specialization) {
+    $sql .= " AND Spécialité = '$specialization'";
+}
 
-	if ($search) {
-		$sql .= " AND (Nom LIKE '%$search%' OR Prénom LIKE '%$search%')";
-	}
+// tri par ordre croissant du prénom
+$sql .= " ORDER BY Prénom ASC";
 
-	if ($specialization) {
-		$sql .= " AND Spécialité = '$specialization'";
-	}
+// executer la requête SQL
+$result = mysqli_query($db_handle, $sql);
 
-	$result = $conn->query($sql);
+$output = '';
 
-	$output = '';
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $output .= '<div class="search-result">';
+        if ($row['Photo']) {
+            // chemin pour les PDP des médecins
+            $output .= '<img src="../13_medecins/' . $row['Photo'] . '" alt="Photo de ' . $row['Prénom'] . ' ' . $row['Nom'] . '" style="float: left; margin-right: 20px;" height="100" width="100">';
+        }
+        $output .= '<div style="display: flex; flex-direction: column; justify-content: center;">';
+        $output .= '<h3><a href="medecin.php?id=' . $row['ID_Médecin'] . '">' . $row['Prénom'] . ' ' . $row['Nom'] . '</a></h3>';
+        $output .= '<p>' . $row['Spécialité'] . '</p>';
+        $output .= '<p>Bureau: ' . $row['Bureau'] . '</p>';
+        $output .= '<p>Téléphone: ' . $row['Téléphone'] . '</p>';
+        $output .= '<p>Email: ' . $row['Email'] . '</p>';
+        $output .= '</div>';
+        $output .= '</div>';
+    }
+} else {
+    $output = '<p>Aucun résultat trouvé</p>';
+}
 
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			$output .= '<div class="search-result">';
-			$output .= '<h3><a href="medecin.php?id=' . $row['ID_Médecin'] . '">' . $row['Prénom'] . ' ' . $row['Nom'] . '</a></h3>';
-			$output .= '<p>' . $row['Spécialité'] . '</p>';
-			$output .= '</div>';
-		}
-	} else {
-		$output = '<p>Aucun résultat trouvé</p>';
-	}
+echo $output;
 
-	echo $output;
-
-	$conn->close();
+// Fermeture de la connexion
+mysqli_close($db_handle);
 ?>
