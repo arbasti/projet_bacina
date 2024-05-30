1b78dcc22b3bdbe12475d7c9ad15ab357920cb7a
Tim <131018615+TimotheeMariotte@users.noreply.github.com>
2024-05-30 15:16:10 +0200

    Update config.php

--- fichier_php/config.php
+++ fichier_php/config.php
@@ -1,8 +1,22 @@
 <?php
+
 	// On démarre la session
 	session_start();
 	if (!isset($_SESSION['id']))
 	{
 		$_SESSION['id'] = NULL; // Définir la valeur par défaut (NULL) si elle n'est pas définie
 	}
+
+    // identifierBDD utilisée
+    $database = "medicare_BDD";
+
+    // Connexion à notre BDD
+    $db_handle = mysqli_connect('localhost', 'root', '');
+    $db_found = mysqli_select_db($db_handle, $database);
+
+    // verif connexion
+    if (!$db_found) {
+        die("Database not found");
+    }
+
 ?>
