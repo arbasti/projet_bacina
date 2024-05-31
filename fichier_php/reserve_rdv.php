2551f02ba2a16ebf27b3f168ebfb302662d3c3b9
Tim <131018615+TimotheeMariotte@users.noreply.github.com>
2024-05-31 23:53:38 +0200

    Update reserve_rdv.php

--- fichier_php/reserve_rdv.php
+++ fichier_php/reserve_rdv.php
@@ -1,19 +1,19 @@
 <?php
-include '../fichier_php/db_connection.php';  // Assurez-vous de remplacer par le fichier de connexion à la base de données
+include '../fichier_php/config.php';  // Assurez-vous de remplacer par le fichier de connexion à la base de données
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $slot = $_POST['slot'];
     $medecin_id = $_POST['medecin_id'];
 
-    // Insérer le rendez-vous dans la table `rendezvous`
-    $sql = "INSERT INTO rendezvous (Date, Heure, ID_Médecin, Statut, Moyen_communication) VALUES (?, ?, ?, 'Confirmé', 'Texte')";
+    // Insérer le rendez-vous dans la table `rdvmedecin`
+    $sql = "INSERT INTO rdvmedecin (datee, heure, ID_Medecin, statut, note) VALUES (?, ?, ?, 'Confirmé', 'Rendez-vous en ligne')";
     if ($stmt = $db_handle->prepare($sql)) {
         list($jour, $heure) = explode(' ', $slot);
         $date = date('Y-m-d', strtotime("next $jour"));
-        $stmt->bind_param("sssi", $date, $heure, $medecin_id);
+        $stmt->bind_param("ssi", $date, $heure, $medecin_id);
         if ($stmt->execute()) {
             // Mettre à jour les disponibilités du médecin
-            $sql_update = "UPDATE médecin SET Disponibilité = JSON_REMOVE(Disponibilité, '$[\"$slot\"]') WHERE ID_Médecin = ?";
+            $sql_update = "UPDATE medecin SET Disponibilité = JSON_REMOVE(Disponibilité, '$[\"$slot\"]') WHERE ID_Medecin = ?";
             if ($stmt_update = $db_handle->prepare($sql_update)) {
                 $stmt_update->bind_param("i", $medecin_id);
                 $stmt_update->execute();
